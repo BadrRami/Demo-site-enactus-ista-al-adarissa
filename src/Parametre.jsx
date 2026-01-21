38d158e31198b80ab19f51427a1faa0c80624385
BADR RAMI <badrrami2004@gmail.com>
2026-01-21 12:49:33 +0100

    hash

--- src/Parametre.jsx
+++ src/Parametre.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import LeftBar from './LeftBar';
 import { useNavigate } from 'react-router-dom';
 import supabase from './SupaBase';
-import bcrypt from 'bcryptjs'; // <-- Import bcrypt
+//import bcrypt from 'bcryptjs'; // <-- Import bcrypt
 
 const Parametre = () => {
   const [user, setUser] = useState(null);
